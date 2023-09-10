f5cd4aaf75a4fc1e0b609c7195419798f49baf85
sxm26790 <124528330+sxm26790@users.noreply.github.com>
2023-09-10 14:08:26 -0500

    Update ThingTest.java

--- src/test/java/com/geekcap/vmturbo/ThingTest.java
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -1,18 +1,33 @@
 package com.geekcap.vmturbo;
 
-import org.junit.Assert;
+/**import org.junit.Assert;
+import org.junit.Test;
+*/
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 
 /**
  * Created by shaines on 7/24/16.
  */
-public class ThingTest {
+/**public class ThingTest {
 
-    @Test
-    public void testN() {
-        Thing t = new Thing();
-        t.setN(5);
-        Assert.assertEquals("N should be 5", 5, t.getN());
+    *@Test
+   * public void testN() {
+        *Thing t = new Thing();
+        *t.setN(5);
+        *Assert.assertEquals("N should be 5", 5, t.getN());
+    *}
+*} */
+public class Thing {
+    private int n;
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
     }
 }
+
